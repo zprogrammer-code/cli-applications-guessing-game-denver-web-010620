9cea70549ad84385eca87813acb81146769ce0e3
Zachary Jacobs <skatboyza124@hotmail.com>
2020-01-07 02:42:29 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -1,19 +1,15 @@
+require 'pry'
 def run_guessing_game
  number = rand(6) + 1
  prompt = puts "guess a number between 1 and 6\n"
  input = gets.chomp 
- 
- if  input == 'exit'
-   print "Goodbye!"
+ if input == 'exit'
+   puts "Goodbye!"
  end
- condition1 = prompt
-  if number != input
-   puts "Sorry! The computer input is equal to #{number}."
-end
-prompt
- if number == input
+  if input == number
    puts "You guessed the correct number!"
-
+  else input != number
+   puts "Sorry! The computer input is equal to #{number}."
+    prompt = puts "guess a number between 1 and 6\n"
+   end 
  end
-  end 
-
