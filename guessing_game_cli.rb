f0e022a3b47e0e88866c30c3835f5402298b93e9
Zachary Jacobs <skatboyza124@hotmail.com>
2020-01-07 03:01:34 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -3,7 +3,7 @@ def run_guessing_game
  number = rand(6) + 1
  prompt = puts "guess a number between 1 and 6\n"
  input = gets.chomp.to_i
- if input == 'exit'
+ if input == 'exit'.to_i
    puts "Goodbye!"
  end
   if input == number
